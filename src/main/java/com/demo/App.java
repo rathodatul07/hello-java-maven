3096b14683a2b5441dc8e2c304ab45bb75302cae
assertion <assertion@localhost.localdomain>
2025-12-29 13:02:45 +0530

    Atul Ajay are Freind

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Atul...Is there any vacancies in your compony?");
+        System.out.println("Hello Shrikanth");
     }
 }
