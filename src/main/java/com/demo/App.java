022562b9938e639242dacc6911693a7fa5a0b985
assertion <assertion@localhost.localdomain>
2025-12-29 12:42:59 +0530

    Hello Atul...Is there any vacancies in your compony?

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Atul Rathod");
+        System.out.println("Hello Atul...Is there any vacancies in your compony?");
     }
 }
